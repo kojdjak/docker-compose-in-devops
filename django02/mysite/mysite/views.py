f5211146001bb1e5ec3c6037537c4f93deacd186
Juraj Kojdjak <juraj.kojdjak@gmail.com>
2015-03-24 14:55:13 +0000

    add space

--- django02/mysite/mysite/views.py
+++ django02/mysite/mysite/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the mysite index in example02.")
+    return HttpResponse("Hello, world. You're at the mysite index in example 02.")
